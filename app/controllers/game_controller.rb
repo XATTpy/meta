d6bf05a4ae21c1b5aaf404cd2fb86191f713fb8d
XATTpy <zoomerrr@mail.ru>
2019-04-26 01:27:28 +0300

    bugs fixed

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,8 @@
 class GameController < ApplicationController
   def sum
+    if params['text'] != nil
+      return redirect_to "/?text=#{params['text']}"
+    end
     id = params['id']
     @game = (@game = Game.where("id == #{id}"))[0]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'game/sum'
   get 'games/index'
+  get 'sum/:text', to: 'games#index', as: :text
 
   root 'games#index'
 end
