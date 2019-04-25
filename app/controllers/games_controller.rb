1b1d94bc8e94fe213e5fb3e0b96f9c27003f0cbc
XATTpy <zoomerrr@mail.ru>
2019-04-25 16:19:21 +0300

    added dropdown "top100"

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,15 @@ class GamesController < ApplicationController
     @page = params['page'].to_i
     start = @page * 100
     finish = start + 100
-    @games = Game.where("id > #{start} AND id <= #{finish}")
+    if params['filter'] == 'top100'
+      if params['platform'] != nil
+        plat = params['platform']
+        @games = Game.joins(:platform).where("platform == '#{plat}'").order(score: :desc).first(100)
+      else
+        @games = Game.order(score: :desc).first(100)
+      end
+    else
+      @games = Game.where("id > #{start} AND id <= #{finish}")
+    end
   end
 end
